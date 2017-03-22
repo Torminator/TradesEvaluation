a72426b7b28265eeaf5390365e18f0da2c50b7b1
Torben Schick <torben.schick@uni-jena.de>
2017-03-23 00:14:11 +0100

    added 3-team trade support, therefore some code restructure

--- scrapeTrades.py
+++ scrapeTrades.py
@@ -7,25 +7,27 @@ class  Trade:
 
     # the class has an attribute for the name of both teams
     # and a list of assets which they traded
-    def __init__(self, team1, team2, team1_assets, team2_assets):
-        self.__tradeid = None
-        self.__team1 = team1
-        self.__team2 = team2
-        self.__team1_assets = team1_assets
-        self.__team2_assets = team2_assets
+    def __init__(self, team, inflow=None, outflow=None):
+        self.__team = team
+        if inflow is None:
+            self.__inflow = []
+        else:
+            self.__inflow = inflow
+        if outflow is None:
+            self.__outflow = []
+        else:
+            self.__outflow = outflow
         self.__date = None
+        self.__tradeid = None
 
-    def getTeam1(self):
-        return self.__team1
+    def getTeam(self):
+        return self.__team
 
-    def getTeam2(self):
-        return self.__team2
+    def getInflow(self):
+        return self.__inflow
 
-    def getTeam1Assets(self):
-        return self.__team1_assets
-
-    def getTeam2Assets(self):
-        return self.__team2_assets
+    def getOutflow(self):
+        return self.__outflow
 
     def getDate(self):
         return self.__date
@@ -35,6 +37,14 @@ class  Trade:
 
     def setTradeID(self, tradeid):
         self.__tradeid = tradeid
+
+    def addInflow(self, assets):
+        for asset in assets:
+            self.__inflow.append(asset)
+
+    def addOutflow(self, assets):
+        for asset in assets:
+            self.__outflow.append(asset)
 
     def __str__(self):
         return self.__team1 + " - " + self.__team2
@@ -48,7 +58,56 @@ def parseTrade(array):
         # we only need the index of 'to the' to partition the sentence correctly
         index = str(array[i]).find("to the")
         if index > -1:
-           return Trade(array[1].contents[0], array[i+1].contents[0], parseAssets(array[2:i+1]), parseAssets(array[i+2:-1]))
+            assets_1 = parseAssets(array[2:i+1])
+            assets_2 = parseAssets(array[i+2:-1])
+            return [Trade(array[1].contents[0], assets_2, assets_1), Trade(array[i+1].contents[0], assets_1, assets_2)]
+    return []
+
+# this function handles 3-teams trades
+def parse3TeamTrade(array):
+    # in a 3-team trade every transaction is listed, for example
+    #   Atlanta Hawks traded Jeff Teague to the Indiana Pacers;
+    # so we want to get the position of traded and to the
+    # with that knowledge we know the position of the teams and the assets
+    trade_index = []
+    for idx,val in enumerate(array):
+        if isinstance(val, str) and val.find("traded") != -1:
+            start = idx
+        if isinstance(val, str) and val.find("to the") != -1:
+            end = idx
+            trade_index.append((start, end))
+
+    # after we got the start and end indices we need to get the idx of the teams
+    # to add the assets to the correct team
+    teams = []
+    for index in trade_index:
+        t1_idx = isCreated(array[index[0]-1].contents[0], teams)
+        t2_idx = isCreated(array[index[1]+1].contents[0], teams)
+        # if it is not created then we need to create it
+        if t1_idx == -1:
+            teams.append(Trade(array[index[0]-1].contents[0]))
+            t1_idx = len(teams)-1
+        # if it is not created then we need to create it
+        if t2_idx == -1:
+            teams.append(Trade(array[index[1]+1].contents[0]))
+            t2_idx = len(teams)-1
+
+        # get the assets with our function parseAssets
+        # we use the knowledge about the position to slice the array correctly
+        assets = parseAssets(array[index[0]:index[1]])
+
+        # for the first team it loses the assets
+        teams[t1_idx].addOutflow(assets)
+        # and the second team receives the assets
+        teams[t2_idx].addInflow(assets)
+
+    return teams
+
+def isCreated(teamname, teams):
+    for idx, team in enumerate(teams):
+        if teamname == team.getTeam():
+            return idx
+
     return -1
 
 def commaAndSplitting(text):
@@ -108,7 +167,7 @@ def parseAssets(array):
 if __name__ == '__main__':
 
     # use requests to get the website
-    r = requests.get("http://www.basketball-reference.com/leagues/NBA_2016_transactions.html")
+    r = requests.get("http://www.basketball-reference.com/leagues/NBA_2017_transactions.html")
 
     # use BeautifulSoup to parse the html
     soup = BeautifulSoup(r.content, "html5lib")
@@ -137,20 +196,25 @@ if __name__ == '__main__':
             # serach explicitly for the keyword 'traded'
             if deal.contents[2].find("traded") > -1:
                 # call the parseTrade function and add the returning Trade object
-                trade = parseTrade(deal.contents)
-                if trade == -1:
+                if deal.contents[0].find("3-team trade") != -1:
+                    trade_parts = parse3TeamTrade(deal.contents)
+                else:
+                    trade_parts = parseTrade(deal.contents)
+
+                if not trade_parts:
                     print("ERROR")
                 else:
                     # set the date of the trade
-                    trade.setDate(date)
-                    # set the id of the trade
-                    trade.setTradeID("{}.{}".format(idx, trade.getDate().split(",")[-1].strip()))
-                    trades.append(trade)
-                    idx += 1
+                    for trade in trade_parts:
+                        trade.setDate(date)
+                        # set the id of the trade
+                        trade.setTradeID("{}.{}".format(idx, trade.getDate().split(",")[-1].strip()))
+                        trades.append(trade)
+                        idx += 1
 
     # create a json string of our scraped data
     json_data = json.dumps([trade.__dict__ for trade in trades])
 
     # storing the data in a json file
-    with  open("trades_2016.json", "w+") as file:
+    with  open("trades_2017_new.json", "w+") as file:
         json.dump(json_data, file)
